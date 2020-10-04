0d85c9e079b0d0f120f10342a1481d09ea61ef7a
Anatoly Zavarukhin <35347611+Innsmouth-trip@users.noreply.github.com>
2020-10-05 00:51:47 +0300

    Add files via upload

---
+++ check_page.py
@@ -0,0 +1,21 @@
+from urllib.error import HTTPError
+from urllib.request import urlopen
+
+with open('url.txt', 'r') as text_file:
+    url_list = text_file.readlines()
+
+for line in url_list:
+    url = line.replace(
+        'https://polyglass.ru', 'http://poly.raz-vitie.ru'
+    ).strip()
+
+    print(str(url_list.index(line) + 1) + ' of ' + str(len(url_list)))
+    print('---')
+    print(url)
+
+    try:
+        print(urlopen(url))
+            
+    except HTTPError:
+        with open('url_404.txt', 'a+') as results:
+            results.write(url + '\n')
